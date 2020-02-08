d0e052b333cc16c5adae98d57f33fa720f6bf99d
Joseph Pietroski <fast3212@gmail.com>
2020-02-08 15:40:53 -0500

    2
    asd

--- Practical.py
+++ Practical.py
@@ -1,2 +1,15 @@
-print("Practial Test")
-sleep(100)
+#!/bin/python3
+import os
+import logging
+import subprocess
+attack1 = 'Attack1.py'
+while True:
+    attack = int(input('Choose Attack:  '))
+
+    
+    if attack <2:
+        print("SYN Scan")
+        subprocess.call('Attack1.py', shell=True)
+    else:
+        print('This is Not An Attack')
+print('YOU GET A ZEROOOO')
