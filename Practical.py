c3ca8a2a8866264137dd65840057f2b11657a474
Fast3212 <fast3212@gmail.com>
2020-02-09 16:01:35 -0500

    Update Practical.py

--- Practical.py
+++ Practical.py
@@ -1,15 +1,54 @@
-#!/bin/python3
 import os
 import logging
 import subprocess
+from scapy.all import *
 attack1 = 'Attack1.py'
+
+
 while True:
     attack = int(input('Choose Attack:  '))
-
-    
-    if attack <2:
+    ip = str(input("What is the IP of the Attacker:  "))
+ 
+    if attack == 1:
         print("SYN Scan")
-        subprocess.call('Attack1.py', shell=True)
+        subprocess.call(f"nmap -p 22 -sS {ip}", shell=True)
+    elif attack == 2:
+        print("Fin Scan")
+        subprocess.call(f"nmap -sF -p 80 {ip}", shell=True)
+    elif attack == 3:
+        print("TCP Connect")
+        subprocess.call(f"nmap {ip} -p 22 -sT", shell=True)
+    elif attack == 4:
+        print("XMAS Port Scan")
+        subprocess.call(f"nmap -sX -p 80 {ip}", shell=True)
+    elif attack == 5:
+        print("NULL Port Scan")
+        subprocess.call(f"nmap -sN -p 80 {ip}", shell=True)
+    elif attack == 6:
+        print("ACK Port Scan")
+        subprocess.call(f"nmap -p 22 -sA {ip}", shell=True)
+    elif attack == 7:
+        print("Idle Port Scan")
+        # subprocess.call("nmap -p 22 -sA 10.0.0.2", shell=True)
+    elif attack == 8:
+        print("UDP Port Scan")
+        subprocess.call(f"nmap -sU {ip} -p 138", shell=True)
+    elif attack == 9:
+        print("Active OS Fingerprinting")
+        subprocess.call(f"nmap -p 80 -O {ip}", shell=True)
+    elif attack == 10:
+        print("IP Spoofing")
+        subprocess.call(f"hping3 -S {ip} -a 192.168.20.5 -c 3 ", shell=True)
+    elif attack == 12:
+        print("DDOS: Syn Floods")
+        subprocess.call(f"hping3 -c 10000 -d 120 -S -w 64 -p 80 --flood {ip} ", shell=True)
+    elif attack == 13:
+        print("NMAP ICMP Ping")
+        subprocess.call(f"ping {ip} -s 0 " , shell=True)
+    elif attack == 14:
+        print("Active OS Fingerprinting")
+        subprocess.call(f"nmap -p 80 -O {ip}", shell=True)
     else:
-        print('This is Not An Attack')
-print('YOU GET A ZEROOOO')
+        print("Blyat!@#")
+        
+
